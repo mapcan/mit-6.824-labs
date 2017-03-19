2bf640c5e06153e2fd3b649cc350c33a905e0b9f
Zhang Ke <zhangke8429@gmail.com>
2017-03-20 01:12:55 +0800

    pass basic

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,11 +3,16 @@ package raftkv
 import "labrpc"
 import "crypto/rand"
 import "math/big"
-
+import "sync"
+import "fmt"
+import "io"
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	id       string
+	sequence int64
+	mu       sync.Mutex
 }
 
 func nrand() int64 {
@@ -17,10 +22,25 @@ func nrand() int64 {
 	return x
 }
 
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
+
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.id, _ = newUUID()
+	ck.sequence = 0
 	return ck
 }
 
@@ -37,9 +57,22 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
-	// You will have to modify this function.
-	return ""
+	var request GetArgs
+	ck.mu.Lock()
+	request.ClientID = ck.id
+	request.Sequence = ck.sequence
+	ck.sequence++
+	request.Key = key
+	ck.mu.Unlock()
+	for {
+		for _, server := range ck.servers {
+			var reply GetReply
+			ok := server.Call("RaftKV.Get", &request, &reply)
+			if ok && !reply.WrongLeader {
+				return reply.Value
+			}
+		}
+	}
 }
 
 //
@@ -54,6 +87,28 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	var request PutAppendArgs
+	ck.mu.Lock()
+	request.ClientID = ck.id
+	request.Sequence = ck.sequence
+	ck.sequence++
+	request.Key = key
+	request.Value = value
+	request.Op = op
+	ck.mu.Unlock()
+	for {
+		for i, server := range ck.servers {
+			var reply PutAppendReply
+			//DPrintf("Server: %d, PutAppendArgs: %v", i, request)
+			ok := server.Call("RaftKV.PutAppend", &request, &reply)
+			//DPrintf("Server: PutAppendReply: %v", i, reply)
+			if ok && !reply.WrongLeader {
+				DPrintf("Server %d, Key: %s, Value: %s, PutAppendArgs: %v", i, request.Key, request.Value, request)
+				DPrintf("Server %d, Key: %s, Value: %s, PutAppendReply: %v", i, request.Key, request.Value, reply)
+				return
+			}
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -6,9 +6,10 @@ import (
 	"log"
 	"raft"
 	"sync"
+	"time"
 )
 
-const Debug = 0
+const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
@@ -17,11 +18,15 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	ClientID  string
+	Sequence  int64
+	Operation string
+	Key       string
+	Value     string
 }
 
 type RaftKV struct {
@@ -33,15 +38,105 @@ type RaftKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	database     map[string]string
+	detectDup    map[string]int64
+	notifyCommit map[int]chan int
 }
-
 
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	cmd := Op{
+		ClientID:  args.ClientID,
+		Sequence:  args.Sequence,
+		Operation: "Get",
+		Key:       args.Key,
+	}
+	index, _, isLeader := kv.rf.Start(cmd)
+	if !isLeader {
+		reply.WrongLeader = true
+		reply.Err = "WrongLeader"
+		return
+	}
+	DPrintf("Server %d, Get Key: %s", kv.me, args.Key)
+	kv.mu.Lock()
+	ch, ok := kv.notifyCommit[index]
+	if !ok {
+		ch = make(chan int, 1)
+		kv.notifyCommit[index] = ch
+	}
+	kv.mu.Unlock()
+	select {
+	case commitIndex := <-ch:
+		if commitIndex == index {
+			kv.mu.Lock()
+			reply.Value = kv.database[cmd.Key]
+			DPrintf("Server %d, Get Key: %s, Get Value: %s, database: %v", kv.me, cmd.Key, reply.Value, kv.database)
+			delete(kv.notifyCommit, index)
+			kv.mu.Unlock()
+		} else {
+			reply.Err = "Commit Out of Order"
+		}
+	case <-time.After(1000 * time.Millisecond):
+		reply.Err = "Operation Timedout"
+	}
 }
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	cmd := Op{
+		ClientID:  args.ClientID,
+		Sequence:  args.Sequence,
+		Operation: args.Op,
+		Key:       args.Key,
+		Value:     args.Value,
+	}
+	//DPrintf("PutAppend Args: %v", args)
+	index, _, isLeader := kv.rf.Start(cmd)
+	//DPrintf("index: %d, isLeader: %v", index, isLeader)
+	if !isLeader {
+		reply.WrongLeader = true
+		reply.Err = "WrongLeader"
+		return
+	}
+	DPrintf("Server %d, PutAppend Key: %s, Value: %s, Index: %d, Args: %v", kv.me, args.Key, args.Value, index, args)
+	kv.mu.Lock()
+	ch, ok := kv.notifyCommit[index]
+	if !ok {
+		ch = make(chan int, 1)
+		kv.notifyCommit[index] = ch
+	}
+	kv.mu.Unlock()
+	select {
+	case commitIndex := <-ch:
+		DPrintf("Receive index %d from Channel %v", commitIndex, ch)
+		if commitIndex == index {
+			reply.Err = OK
+			kv.mu.Lock()
+			v, ok := kv.detectDup[cmd.ClientID]
+			if !ok || v < cmd.Sequence {
+				switch {
+				case cmd.Operation == "Append":
+					kv.database[cmd.Key] += cmd.Value
+					DPrintf("Server %d, Append Key: %s, Value: %s, database: %v", kv.me, cmd.Key, cmd.Value, kv.database)
+				case cmd.Operation == "Put":
+					kv.database[cmd.Key] = cmd.Value
+					DPrintf("Server %d, Put Key: %s, Value: %s, database: %v", kv.me, cmd.Key, cmd.Value, kv.database)
+				default:
+					reply.Err = "Invalid Operation"
+				}
+				kv.detectDup[cmd.ClientID] = cmd.Sequence
+			} else {
+				DPrintf("Detected Duplicate, ok: %v, v: %d, cmd.Sequence: %d", ok, v, cmd.Sequence)
+			}
+			delete(kv.notifyCommit, index)
+			kv.mu.Unlock()
+		} else {
+			reply.Err = "Commit Out of Order"
+		}
+		//case <-time.After(2000 * time.Millisecond):
+		//	DPrintf("Server: %d, Operation Timedout, PutAppend Key: %s, Value: %s, Index: %d, Args: %v", kv.me, args.Key, args.Value, index, args)
+		//	reply.Err = "Operation Timedout"
+	}
+	//DPrintf("PutAppend reply: %v", reply)
 }
 
 //
@@ -78,11 +173,30 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// You may need initialization code here.
+	kv.detectDup = make(map[string]int64)
+	kv.database = make(map[string]string)
+	kv.notifyCommit = make(map[int]chan int)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	go func() {
+		for {
+			msg := <-kv.applyCh
+			DPrintf("Server %d, ApplyMsg: %v", kv.me, msg)
+			index := msg.Index
+			kv.mu.Lock()
+			ch, ok := kv.notifyCommit[index]
+			if !ok {
+				ch = make(chan int, 1)
+				kv.notifyCommit[index] = ch
+			}
+			kv.mu.Unlock()
+			DPrintf("Server %d, Send index %d to Channel %v", kv.me, index, ch)
+			ch <- index
+		}
+	}()
 
 	return kv
 }
